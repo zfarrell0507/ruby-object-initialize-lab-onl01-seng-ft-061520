d305243bce0ec7638659f997eb43390d573e369d
Zachary Farrell <zfarrell0507@gmail.com>
2020-06-28 00:15:50 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,5 +1,5 @@
 class Dog
-  def initialize(name, breed) || initialize(name)
+  def initialize(name, breed = "Mutt")
     @name = name
     @breed = breed
   end
@@ -14,23 +14,6 @@ class Dog
   
   def breed=(breed)
       @breed = breed
-  end
-
-  def initialize(name)
-    @name = name
-    @breed = breed
-  end
-   
-  def name=(name)
-    @name = name
-  end
- 
-  def name
-    @name
-  end
-  
-  def breed=(breed)
-      @breed = "Mutt"
   end
 end
 natalie = Dog.new("Natalie", "Black Lab")
